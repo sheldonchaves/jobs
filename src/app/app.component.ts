9b67b2894866125698dffafce146b686d291a2de
Sheldon Chaves <sheldonchaves@gmail.com>
2023-03-19 11:33:33 -0300

    flow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  OnDestroy,
+  OnInit,
+  HostListener,
+  Inject,
+} from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { ChildrenOutletContexts } from '@angular/router';
+import {
+  trigger,
+  transition,
+  query,
+  style,
+  animate,
+  animateChild,
+  group,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('myAnimation', [
+      transition('* => *', [
+        style({ position: 'relative' }),
+        query(
+          ':enter, :leave',
+          [
+            style({
+              position: 'absolute',
+              top: 0,
+              right: 0,
+              width: '100%',
+            }),
+          ],
+          { optional: true }
+        ),
+        query(':enter', [style({ right: '-100%', opacity: 0 })], {
+          optional: true,
+        }),
+        query(':leave', animateChild()),
+        group([
+          query(':leave', [
+            animate('.3s ease-out', style({ right: '100%', opacity: 0 })),
+          ]),
+          query(':enter', [
+            animate('.3s ease-out', style({ right: '0%', opacity: 1 })),
+          ]),
+        ]),
+        query(':enter', animateChild()),
+      ]),
+    ]),
+  ],
 })
 export class AppComponent {
-  title = 'idt';
+  constructor(private contexts: ChildrenOutletContexts) {}
 }
---
+++ src/app/comps/header/header.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { Location } from '@angular/common';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+})
+export class HeaderComponent {
+  constructor(private location: Location) {}
+
+  back() {
+    this.location.back();
+  }
+}
